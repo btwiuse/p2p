55a93712c0259fdbf76d47b288f466e3120edfa1
navigaid <navigaid@gmail.com>
2024-05-07 13:40:34 +0000

    add noise connection encryption

--- tcp/index.mjs
+++ tcp/index.mjs
@@ -1,17 +1,37 @@
-import { createLibp2p } from 'libp2p'
-import { tcp } from '@libp2p/tcp'
+import { createLibp2p } from "libp2p";
+import { tcp } from "@libp2p/tcp";
+import { ping } from "@libp2p/ping";
+import { identify, identifyPush } from "@libp2p/identify";
+import { yamux } from "@chainsafe/libp2p-yamux";
+import { webSockets } from "@libp2p/websockets";
+import { multiaddr } from "@multiformats/multiaddr";
+import { noise } from "@chainsafe/libp2p-noise";
 
 const node = await createLibp2p({
-	addresses: {
-		listen: ['/ip4/127.0.0.1/tcp/3233']
-	},
-	transports: [tcp()],
-})
+  addresses: {
+    listen: [
+      "/ip4/127.0.0.1/tcp/31337/ws",
+    ],
+  },
+  connectionEncryption: [noise()],
+  transports: [
+    tcp(),
+    webSockets(),
+  ],
+  services: {
+    ping: ping({
+      protocolPrefix: "ipfs", // default
+    }),
+    identify: identify(),
+    identifyPush: identifyPush(),
+  },
+  streamMuxers: [yamux()],
+});
 
-await node.start()
-console.log('libp2p has started')
+await node.start();
+console.log("libp2p has started");
 
-console.log('listening on addresses:')
+console.log("listening on addresses:");
 node.getMultiaddrs().forEach((addr) => {
-  console.log(addr.toString())
-})
+  console.log(addr.toString());
+});

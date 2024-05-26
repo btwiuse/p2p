0e471641d12f159ead0037d6c59f6865f3f6abfe
navigaid <navigaid@gmail.com>
2024-05-26 18:22:55 +0000

    add dial

---
+++ dial/index.mjs
@@ -0,0 +1,58 @@
+import { createLibp2p } from "libp2p";
+import { echo } from "@libp2p/echo";
+import { ping } from "@libp2p/ping";
+import { identify, identifyPush } from "@libp2p/identify";
+import { yamux } from "@chainsafe/libp2p-yamux";
+import { webSockets } from "@libp2p/websockets";
+import { multiaddr } from "@multiformats/multiaddr";
+import { noise } from "@chainsafe/libp2p-noise";
+
+const node = await createLibp2p({
+  addresses: {
+    listen: [
+      "/ip4/127.0.0.1/tcp/31337/ws",
+    ],
+  },
+  connectionEncryption: [noise()],
+  transports: [
+    webSockets(),
+  ],
+  services: {
+    echo: echo(),
+    ping: ping({
+      protocolPrefix: "ipfs", // default
+    }),
+    identify: identify(),
+    identifyPush: identifyPush(),
+  },
+  streamMuxers: [yamux()],
+});
+
+await node.start();
+console.log("libp2p has started");
+
+console.log("listening on addresses:");
+node.getMultiaddrs().forEach((addr) => {
+  console.log(addr.toString());
+});
+
+// get argument 1
+if (process.argv.length < 3) {
+  console.error("Usage: node index.mjs <dest>");
+  process.exit(1);
+}
+let dest = multiaddr(process.argv[2]);
+
+node.addEventListener("peer:discovery", (evt) => {
+  console.log("Discovered %s", evt.detail.id.toString()); // Log discovered peer
+});
+
+node.addEventListener("peer:connect", (evt) => {
+  console.log("Connected to %s", evt.detail.toString()); // Log connected peer
+});
+
+node.addEventListener("peer:disconnect", (evt) => {
+  console.log("Disconnected from %s", evt.detail.toString()); // Log disconnected peer
+});
+
+let conn = await node.dial(dest);
